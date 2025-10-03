31bdbed7de6cb8f3d2b28a46ee036b061ae13b40
takipsi-lim <limgloriabusiness.ph@gmail.com>
2025-10-03 15:09:19 +0800

    Create shapes.c

---
+++ shapes.c
@@ -0,0 +1,24 @@
+
+/*
+Hello po, before we start para mas efficient ang programming process natin, 
+below po state your username (Account Name). For formality para malaman natin sino nag-eedit.
+*/
+
+/*
+        Group 4 Members:
+        [Last Name, First Name, – Username]
+        |Gloria, Limuel         – takipsi-lim
+        |Mariano, Prince        – 
+        |Sakai, Tatsuya         – 
+        |Lorenzo, Lexis         – 
+        |Almoguera, John        – 
+*/
+
+//Below is where we start the code po :D
+
+
+#include <stdio.h>
+int main()
+{
+    //Code
+}
